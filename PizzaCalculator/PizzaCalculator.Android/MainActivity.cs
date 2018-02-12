faecd3777d8d6a0cadd3b27c10eaf3133d67a7c6
Alan Mills <aelonrench@yahoo.com>
2018-02-12 10:31:18 -0500

    Add project files.

---
+++ PizzaCalculator/PizzaCalculator.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace PizzaCalculator.Droid
+{
+    [Activity(Label = "PizzaCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var peopleEntry = FindViewById<EditText>(Resource.Id.peopleEntry);
+            var calculate = FindViewById<Button>(Resource.Id.calculateButton);
+            var pizzaCount = FindViewById<TextView>(Resource.Id.pizzaCountLabel);
+
+            calculate.Click += delegate
+            {
+                var count = int.Parse(peopleEntry.Text);
+                var pizzas = count / 3;
+                pizzaCount.Text = pizzas.ToString();
+            };
+        }
+    }
+}
+
---
+++ PizzaCalculator/PizzaCalculator/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PizzaCalculator
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
